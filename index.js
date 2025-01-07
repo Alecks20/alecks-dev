50a1036f79fca0c29c006f7226c1a92cf131f8dc
alecks20 <109296824+Alecks20@users.noreply.github.com.>
2025-01-07 21:38:53 +0800

    Used indian tech support tutorial to fix the assets and started using the actual routes in express.js

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use("/assets",express.static("assets"))
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get("/privacy", (req, res) => {
+    res.render("privacy.ejs");
+});
 
 app.listen(port, () => {
     console.log(`App listening on ${port}`)
