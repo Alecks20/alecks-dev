04252d8cbe062f2baa74e3835b680b5122215242
alecks20 <109296824+Alecks20@users.noreply.github.com.>
2025-05-30 02:59:20 +0800

    Added navigation, styling changes, etc, support for more pages

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.get("/privacy", (req, res) => {
     res.render("privacy.ejs");
 });
 
+app.get("/experience", (req, res) => {
+    res.render("experience.ejs");
+});
+
+
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
 });
